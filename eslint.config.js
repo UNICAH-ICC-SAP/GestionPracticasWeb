2288c8fb5d9a7a265b2c2d38fa1064fecb3a3f50
Ariel Calix <acalixv@unicah.edu>
2024-11-13 03:41:48 -0600

    chore: configuracion de commitlint, eslint y husky

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
+  { languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-namespace": 'off',
+    }
+  },
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      "react": {
+        "version": "detect"
+      }
+    }
+  }
+];
