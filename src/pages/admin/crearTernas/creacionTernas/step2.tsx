4bd6692e7b698c28ae342bf2b9d2f46763874146
Ariel Calix <acalixv@unicah.edu>
2025-01-11 07:23:39 -0600

    fix: control de estados en terna y mejoras en validaciones

--- src/pages/admin/crearTernas/creacionTernas/step2.tsx
+++ src/pages/admin/crearTernas/creacionTernas/step2.tsx
@@ -4,10 +4,12 @@ import { TypeUtilities } from "../../../../utilities/TypeUtilities";
 import { useDispatch, useSelector } from "../../../../store";
 import { Type as TernaType } from '../../../../store/slices/ternas/_namespace'
 import { Fetcher as FetcherDocentes, Selector as SelectorDocentes } from "../../../../store/slices/docentes";
-import { ButtonPrimary, ButtonSecondary } from "../../../../components/shared/buttons";
+import { ButtonDanger, ButtonPrimary, ButtonSecondary } from "../../../../components/shared/buttons";
 import { Tables } from "../../../../components/commons/tables/tables";
 import { Action as ActionTernas, Selector as SelectorTerna } from "../../../../store/slices/ternas";
 import '../index'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 type Docente = {
     docenteId: string;
@@ -19,16 +21,17 @@ const INITDocente: TernaType.DetalleTernaInfo = {
     detalleTernaId: 0,
     ternaId: 0,
     docenteId: '',
-    coordina: ''
+    coordina: 'No'
 }
 
 export default function Step2() {
     const dispatch = useDispatch();
     const [enableButton, setEnableButton] = React.useState<boolean>(false)
     const [hasChange, setHasChange] = React.useState<boolean>(false);
-    const [state, setState] = React.useState<TernaType.DetalleTernaInfo>(INITDocente);
-    const [noIsValid, setNoIsValid] = React.useState<boolean>(false)
     const [hasCoor, setHasCoor] = React.useState<boolean>(false)
+    const [state, setState] = React.useState<TernaType.DetalleTernaInfo>(INITDocente);
+    const [noIsValid, setNoIsValid] = React.useState<boolean>(false);
+    const [isAdded, setIsAdded] = React.useState<boolean>(false);
     const [docentesInfo, setDocentesInfo] = React.useState<Array<Docente>>([])
     const docentes = useSelector(SelectorDocentes.getDocentes)
     const detalleTernas = useSelector(SelectorTerna.getDetalleTernas)
@@ -65,6 +68,7 @@ export default function Step2() {
         }
         if (detalleTernas.length === 3) { setEnableButton(true) }
     }, [detalleTernas])
+
     useEffect(() => {
         if (detalleTernas.length === 3) {
             const items: Array<Docente> = detalleTernas.map(detalle => {
@@ -87,34 +91,77 @@ export default function Step2() {
         }
     }, [])
     const handleClick = () => {
-        if (detalleTernas.length <= 2 && state.docenteId !== "") {
-            const ternaInfo: TernaType.DetalleTernaInfo = { ...state };
-            const itemDocente = docentes.find(docente => docente.docenteId === state.docenteId)
-            if (itemDocente) {
-                setDocentesInfo([
-                    ...docentesInfo,
-                    {
-                        docenteId: itemDocente?.docenteId,
-                        nombre: itemDocente?.nombre,
-                        coordina: state.coordina
-                    }
-                ])
+        const added = detalleTernas.find(detalle => state.docenteId === detalle.docenteId);
+        if (!added) {
+            if (detalleTernas.length <= 2 && state.docenteId !== "") {
+                const ternaInfo: TernaType.DetalleTernaInfo = { ...state };
+                const itemDocente = docentes.find(docente => docente.docenteId === state.docenteId)
+                if (itemDocente) {
+                    setDocentesInfo([
+                        ...docentesInfo,
+                        {
+                            docenteId: itemDocente?.docenteId,
+                            nombre: itemDocente?.nombre,
+                            coordina: state.coordina
+                        }
+                    ])
+                }
+                dispatch(ActionTernas.setDetalleTerna(ternaInfo))
+                setState(INITDocente)
+            } else {
+                setNoIsValid(true)
+                setTimeout(() => {
+                    setNoIsValid(false)
+                }, 3000)
             }
-            dispatch(ActionTernas.setDetalleTerna(ternaInfo))
-            setState(INITDocente)
+            if (detalleTernas.length === 3) { setEnableButton(true) }
         } else {
-            setNoIsValid(true)
+            setIsAdded(true)
             setTimeout(() => {
-                setNoIsValid(false)
+                setIsAdded(false)
             }, 3000)
         }
-        if (detalleTernas.length === 3) { setEnableButton(true) }
     };
     const handleClickBack = () => {
         dispatch(ActionTernas.setStep1(true));
     }
     const handleClickNext = () => {
         dispatch(ActionTernas.setResumen(true));
+    }
+    const handleDeleteDocente = (docenteId: string) => {
+        const currentDocentes = docentesInfo.filter(item => {
+            if (item.docenteId !== docenteId) {
+                return item;
+            }
+        })
+        const currentDetalleTernas = detalleTernas.filter(item => {
+            if (item.docenteId !== docenteId) {
+                return item;
+            }
+        })
+        const coordinador = currentDetalleTernas.filter(detalle => detalle.coordina === 'Si')
+        console.log(state, hasChange, hasCoor, coordinador)
+        if (coordinador.length === 0) {
+            setHasChange(false);
+            setHasCoor(false);
+        }
+        if (!currentDetalleTernas) {
+            dispatch(ActionTernas.setNoDroppedData([]));
+            setEnableButton(false);
+        } else {
+            dispatch(ActionTernas.setNoDroppedData(currentDetalleTernas))
+            setEnableButton(false)
+        }
+        if (!currentDocentes) setDocentesInfo([]);
+        else setDocentesInfo(currentDocentes);
+
+    }
+    const renderActions = (docenteId: string) => {
+        return <ButtonGroup>
+            <ButtonDanger onClick={() => handleDeleteDocente(docenteId)}>
+                <FontAwesomeIcon icon={faTrash} />
+            </ButtonDanger>
+        </ButtonGroup>
     }
     return <div className="form-group">
         <h5 className="mb-4">Creacion de Nueva Terna</h5>
@@ -140,16 +187,22 @@ export default function Step2() {
                 name="coordina"
                 type="checkbox"
                 onChange={handleCheck}
+                checked={state.coordina === 'Si'}
             />
             <Label check for="coordina">&nbsp;Es coordinador</Label>
         </InputGroup>
         <Alert isOpen={!hasChange} color="dark">Recuerde selecionar al menos un coordinador</Alert>
         <ButtonSecondary className="mb-4" onClick={handleClick} disabled={enableButton} >Agregar Docente</ButtonSecondary>
-        {docentesInfo.length > 0 && <Tables data={docentesInfo} headers={["Id Docente", "Nombre", "Coordinador"]} firstColumnIndex={0} paginated={false} />}
+        {docentesInfo.length > 0 && <Tables data={docentesInfo.map(item => ({
+            ...item,
+            actions: renderActions(item.docenteId)
+        }))} headers={["Id Docente", "Nombre", "Coordinador", "Acciones"]} firstColumnIndex={0} paginated={false} />}
         <ButtonGroup>
             <ButtonSecondary onClick={handleClickBack}>Atras</ButtonSecondary>
             <ButtonPrimary onClick={handleClickNext} disabled={!enableButton}>Siguiente</ButtonPrimary>
         </ButtonGroup>
+
+        <Alert className="alert-color" color="danger" isOpen={isAdded}>El docente ya fue agregado</Alert>
         <Alert className="alert-color" color="danger" isOpen={noIsValid}>El ingreso no es correcto</Alert>
         <Alert className="alert-color" color="info" isOpen={hasCoor}>Debe seleccionar un coordinador</Alert>
     </div>
