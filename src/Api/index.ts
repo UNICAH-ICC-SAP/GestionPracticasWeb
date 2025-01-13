325f10c9658f2139acb47457ea77fb6653cdc034
Ariel Calix <acalixv@unicah.edu>
2025-01-13 01:05:54 -0600

    feat: actualizacion de iconografia

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -1,5 +1,7 @@
 export enum Image {
-    "logo:main" = "Image/UNICAHLogo.jpeg",
+    "logo:main" = "Image/UNICAHLogo.png",
+    "logo:main-circular" = "Image/UNICAHLogoCircular.png",
+    "logo:main-blanco" = "Image/UNICAHLogoBlanco.png",
 }
 
 export default {
