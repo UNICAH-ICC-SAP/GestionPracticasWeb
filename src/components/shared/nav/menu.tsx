0f4595b8529c104a30590fcc51120b1ec0fda048
Ariel Calix <acalixv@gmail.com>
2025-10-03 12:18:16 -0600

    fix: llamado a componentes en navegacion

--- src/components/shared/nav/menu.tsx
+++ src/components/shared/nav/menu.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Menu } from './types';
-import Docentes from '@pages/admin/index';
-import DocenteMenu from '@pages/admin/docentePage/docenteIndex';
-import CargasMenu from '@pages/admin/cargasPage/cargasIndex';
-import Pensums from '@pages/pensums/';
-import { Home } from '@pages/home/home';
-import Alumno from '@pages/perfilAlumnos/alumno';
-import DocentePerfil from "@pages/perfilDocente/ternas"
-import ConfigEmail from "@pages/admin/configEmail"
-import CargaDocente from '@pages/perfilDocente/cargaDocente';
+import Docentes from '../../../pages/admin/index';
+import DocenteMenu from '../../../pages/admin/docentePage/docenteIndex';
+import CargasMenu from '../../../pages/admin/cargasPage/cargasIndex';
+import Pensums from '../../../pages/pensums/';
+import { Home } from '../../../pages/home/home';
+import Alumno from '../../../pages/perfilAlumnos/alumno';
+import DocentePerfil from "../../../pages/perfilDocente/ternas"
+import ConfigEmail from "../../../pages/admin/configEmail"
+import CargaDocente from '../../../pages/perfilDocente/cargaDocente';
 
 
 export const MenuItems: Array<Array<Menu>> = [
